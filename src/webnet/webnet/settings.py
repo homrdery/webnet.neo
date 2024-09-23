b8195b6d8828c2e4801c2da9f0de8dc61bdebea4
Artem <ar.lubimov@gmail.com>
2024-09-23 20:56:47 +0300

    init

--- src/webnet/webnet/settings.py
+++ src/webnet/webnet/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'webnet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path(BASE_DIR).joinpath("templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'webnet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": "webnet",
+        "USER": "webnet",
+        "PASSWORD": "nooZ4thiefee4The",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
