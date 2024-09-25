5adba595ab0f4f55fa841fdca858eaca219b7edd
Alexey V. Lubmov <avlubimov@gmail.com>
2024-09-25 23:06:14 +0300

    login form

--- src/webnet/accounts/views.py
+++ src/webnet/accounts/views.py
@@ -20,4 +20,4 @@ def login(request):
         else:
             form = LoginForm()
 
-    return render(request, "accounts/login.html", {"form": form})
+    return render(request, "accounts/login.html", context={"form": form})
