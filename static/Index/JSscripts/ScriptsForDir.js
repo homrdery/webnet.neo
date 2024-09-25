274843ec4e5fa3a220cb397d846cfded241223d1
Artem <ar.lubimov@gmail.com>
2024-09-25 17:51:04 +0300

    init

---
+++ static/Index/JSscripts/ScriptsForDir.js
@@ -0,0 +1,100 @@
+function getform () {
+        $('#Form').load('directory/getform.html?action=subAddr', function(responseTxt, statusTxt, jqXHR)
+            {
+                if(statusTxt == 'success'){
+                        $('#addForm').modal();
+                    }
+                    if(statusTxt == 'error'){
+                        alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
+                    }
+                }
+        );
+    };
+
+function deladdr( e, dt, node, config )  {
+        var id = $('#myTable').DataTable.$('tr.selected')[0].id;
+        console.log('Выбран пользователь под id ='+id);
+        $("#Form").load("directory/getform.html?action=delAddr&id="+id, function(responseTxt, statusTxt, jqXHR)
+        {
+            if(statusTxt == 'success'){
+                    $('#addFormdel').modal();
+                }
+                if(statusTxt == 'error'){
+                    alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
+
+                }
+        });
+
+}
+
+function tableinit () {
+    var table = $('#myTable').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+        {
+            text:      '<i class="fa fa-plus"></i>',
+            attr:  {
+                title: 'Add item',
+                id: 'BtAdd'
+                },
+            titleAttr: 'Add item',
+            action: function ( e, dt, node, config ) {
+                alert( 'Button activated' );
+                }
+        },
+        {
+            text:      '<i class="fa fa-pen"></i>',
+            attr:  {
+                title: 'Edit item',
+                id: 'BtEdit'
+                },
+            init: function ( dt, node, config ) {
+                this.disable();
+            },
+            action: function readdr( e, dt, node, config )  {
+                var id = $('#myTable').DataTable.$('tr.selected')[0].id;
+                console.log('Delete user id ='+id);
+                $("#Form").load("directory/getform.html?action=reAddr&id="+id, function(responseTxt, statusTxt, jqXHR)
+                {
+                    if(statusTxt == 'success'){
+                            $('#addFormdel').modal();
+                        }
+                        if(statusTxt == 'error'){
+                            alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
+
+                        }
+                });
+
+            }
+        },
+        {
+            text:      '<i class="fas fa-trash  aria-hidden="true"></i>',
+            attr:  {
+                title: 'Delete item',
+                id: 'BtDelete'
+            },
+
+            init: function ( dt, node, config ) {
+                this.disable();
+            },
+            action:    deladdr
+
+        }
+    ],
+    pageLength: 25,
+    select: true,
+    rowId: 'id',
+    language: {
+        url:"/static/datatables/ru.json"
+        },
+});
+    table.on( 'select deselect', function () {
+        var selectedRows = table.rows( { selected: true } ).count();
+        table.buttons(['#BtEdit']).enable( selectedRows === 1 );
+        table.buttons(['#BtDelete']).enable( selectedRows > 0 );
+    });
+}
+
+$(tableinit);
+
+
