5df8ef19368edc7973769e1874f28d16ca0cc39e
Alexey V. Lubmov <avlubimov@gmail.com>
2024-09-25 22:49:49 +0300

    login form

--- src/webnet/accounts/forms.py
+++ src/webnet/accounts/forms.py
@@ -3,9 +3,9 @@ from crispy_forms.helper import FormHelper
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField("Логин")
-    password = forms.CharField("Пароль")
-    next = forms.CharField("Логин")
+    username = forms.CharField(label="Логин", required=True)
+    password = forms.CharField(label="Пароль", required=True)
+    next = forms.CharField(widget=forms.HiddenInput(), initial="/")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
