cf387cf6728d791e1cc59d3ea26884cb27896839
Alexey V. Lubmov <avlubimov@gmail.com>
2024-09-25 23:05:06 +0300

    login form

--- src/webnet/accounts/views.py
+++ src/webnet/accounts/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import LoginForm
+from django.contrib.auth import login, authenticate
 
 
 # Create your views here.
 
 def login(request):
-    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            next = form.cleaned_data.get('next')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(next)
+        else:
+            form = LoginForm()
+
     return render(request, "accounts/login.html", {"form": form})
