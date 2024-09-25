4751fd1aec4b85572c0df5858c972937926ff757
Artem <ar.lubimov@gmail.com>
2024-09-25 17:28:16 +0300

    init

--- src/webnet/index/views.py
+++ src/webnet/index/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'templates/mainpage.html')
+    return render(request, 'index/mainpage.html')
 
