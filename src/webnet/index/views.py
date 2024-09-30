609b7cc262bbe2f5979491de360c3e990841fe1a
Artem <ar.lubimov@gmail.com>
2024-09-30 21:06:01 +0300

    add static

--- src/webnet/index/views.py
+++ src/webnet/index/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
 
 @login_required
 def index(request):
+    messages.add_message(request, messages.INFO, "Hello world.")
     return render(request, 'index/page.html')
 
