abcaf9bf0ed432ee5b26e9e6da47f7ed15cf887e
Artem <ar.lubimov@gmail.com>
2024-09-30 18:51:37 +0300

    add static

--- src/webnet/accounts/forms.py
+++ src/webnet/accounts/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     username = forms.CharField(label="Логин", required=True)
-    password = forms.CharField(label="Пароль", required=True)
+    password = forms.CharField(label="Пароль", required=True, widget=forms.PasswordInput)
     next = forms.CharField(widget=forms.HiddenInput(), initial="/")
 
     def __init__(self, *args, **kwargs):
@@ -14,14 +14,11 @@ class LoginForm(forms.Form):
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Submit', css_class='btn-primary'))
 
-# class RegForm(UserCreationForm):
-#     email = forms.EmailField()
-#     class Meta:
-#         model = User
-#         Fields = ("username", "email", "password1", "password2")
-    # def save (self, commit=True):
-    #     user = super(RegForm, self).save(commit=False)
-    #     user.email = self.cleaned_data['email']
-    #     if commit:
-    #         user.save()
-    #     return user
+class RegForm(UserCreationForm):
+    email = forms.EmailField()
+    def save (self, commit=True):
+        user = super(RegForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
