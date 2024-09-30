93a9920f8abbb263f9c2e5b85424c7992a537f12
Artem <ar.lubimov@gmail.com>
2024-09-30 18:19:08 +0300

    add static

--- src/webnet/accounts/forms.py
+++ src/webnet/accounts/forms.py
@@ -1,10 +1,8 @@
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout  import Submit
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
-
 class LoginForm(forms.Form):
     username = forms.CharField(label="Логин", required=True)
     password = forms.CharField(label="Пароль", required=True)
@@ -16,18 +14,16 @@ class LoginForm(forms.Form):
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Submit', css_class='btn-primary'))
 
-
 class RegForm(UserCreationForm):
-    username = forms.CharField(label="Логин", required=True)
-    password = forms.CharField(label="Пароль", required=True)
+
     email = forms.EmailField(label="Почта", required=True)
-    next = forms.CharField(widget=forms.HiddenInput(), initial="/")
+
+
 
     class Meta:
-        model = User
+        model =User
         Fields = ("username", "password", "email")
-
-    def save(self, commit=True):
+    def save (self, commit=True):
         user = super(RegForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
         if commit:
