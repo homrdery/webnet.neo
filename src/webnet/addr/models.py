a80077c1f48faedc94f7940f36988dadff6911ab
Artem <ar.lubimov@gmail.com>
2024-10-02 21:28:59 +0300

    add static

--- src/webnet/addr/models.py
+++ src/webnet/addr/models.py
@@ -13,7 +13,7 @@ class dirAddr(models.Model):
 class worker(models.Model):
     mac_addr = models.CharField("mac_addr", max_length=32, null=False, unique=True, blank=False, help_text="mac адрес pc")
     name = models.CharField("name", max_length=30, null=False, blank=False, help_text="фио работника")
-    data = models.JSONField("Данные", null=False, help_text="Доп данные")
+    data = models.JSONField("Данные",  help_text="Доп данные")
 
     def __str__(self):
         return f"Работник {self.id}:{self.mac_addr}:{self.name}:{self.data}"
