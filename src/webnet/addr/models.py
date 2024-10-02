edcb4b9d0b47e2088e3485fa44c42ad3c8bb4b77
Artem <ar.lubimov@gmail.com>
2024-10-02 21:22:58 +0300

    add static

--- src/webnet/addr/models.py
+++ src/webnet/addr/models.py
@@ -10,5 +10,15 @@ class dirAddr(models.Model):
         verbose_name = "Работник"
         verbose_name_plural = "Работники"
 
+class worker(models.Model):
+    mac_addr = models.CharField("mac_addr", max_length=32, null=False, unique=True, blank=False, help_text="mac адрес pc")
+    name = models.CharField("name", max_length=30, null=False, blank=False, help_text="фио работника")
+    data = models.JSONField("Данные", null=False, default=JsonForWorkers, help_text="Доп данные")
+
+    def __str__(self):
+        return f"Работник {self.id}:{self.mac_addr}:{self.name}:{self.data}"
+    class Meta:
+        verbose_name = "Работник"
+        verbose_name_plural = "Работники"
 
 # Create your models here.
