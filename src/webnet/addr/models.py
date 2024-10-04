d48dba053567d54172757ca2db372ed3bb7e9558
Artem <ar.lubimov@gmail.com>
2024-10-04 20:51:53 +0300

    add static

--- src/webnet/addr/models.py
+++ src/webnet/addr/models.py
@@ -12,9 +12,9 @@ class dirAddr(models.Model):
 
 class worker(models.Model):
     mac_addr = models.CharField("mac_addr", max_length=32, null=False, unique=True, blank=False, help_text="mac адрес pc")
-    ip_addr = models.CharField("ip_addr", max_length=30, null=False, blank=False, help_text="ip_addr pc")
-    name = models.CharField("name", max_length=30, null=True, unique=True, blank=False, help_text="name")
-    vendor = models.CharField("vendor", max_length=30, null=True, unique=True, blank=False, help_text="vendor")
+    ip_addr = models.CharField("ip_addr", max_length=30, default="--", null=False, blank=False, help_text="ip_addr pc")
+    name = models.CharField("name", max_length=30, null=True, default="--", blank=False, help_text="name")
+    vendor = models.CharField("vendor", max_length=30, null=True, default="--", blank=False, help_text="vendor")
     def __str__(self):
         return f"Работник {self.id}:{self.mac_addr}:{self.ip_addr}:{self.name}:{self.vendor}"
     class Meta:
