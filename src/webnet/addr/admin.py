279132927013827469acc1b2d7d774a104326110
Artem <ar.lubimov@gmail.com>
2024-10-04 19:24:31 +0300

    add static

--- src/webnet/addr/admin.py
+++ src/webnet/addr/admin.py
@@ -7,3 +7,14 @@ class dirAddr(ModelAdmin):
     list_filter = ("mac_addr", )
 
 # Register your models here.
+@register(Pktreader)
+class PktreaderAdmin(ModelAdmin):
+    # pass
+    list_display = ("mac_addr", "ip_addr", "time")
+    search_fields = ('mac_addr', 'ip_addr')
+
+@register(worker)
+class WorkerAdmin(ModelAdmin):
+    list_display = ("name", "mac_addr", "data")
+    search_fields = ('mac_addr', 'name')
+    list_filter = ("mac_addr", )
