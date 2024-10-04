4cee7de8bfc735ca93334f0917167f09202bd856
Artem <ar.lubimov@gmail.com>
2024-10-04 19:22:54 +0300

    add static

--- src/webnet/addr/admin.py
+++ src/webnet/addr/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib.admin import register, ModelAdmin
+from .models import *
+@register(dirAddr)
+class dirAddr(ModelAdmin):
+    list_display = ("name", "mac_addr")
+    search_fields = ('mac_addr', 'name')
+    list_filter = ("mac_addr", )
 
 # Register your models here.
