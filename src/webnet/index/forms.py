2786e3be5d1767b6f638714315dc2a6205ea8e4e
Artem <ar.lubimov@gmail.com>
2024-10-04 19:15:57 +0300

    add static

--- src/webnet/index/forms.py
+++ src/webnet/index/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-# from .models import worker, Pktreader
+from addr.models import dirAddr, worker, Pktreader
 from django.forms import HiddenInput, ChoiceField
 from crispy_forms.bootstrap import Modal
 from crispy_forms.helper import FormHelper
@@ -9,3 +9,27 @@ import logging
 APPNAME = "client"
 logger = logging.getLogger(APPNAME)
 
+
+def list_mac_addr():
+    box = []
+    for mac in Pktreader.objects.order_by('mac_addr').values_list('mac_addr', flat=True):
+        items = worker.objects.filter(mac_addr = mac).values_list('id')
+        if len(items) == 0:
+            box.append((mac, mac))
+    logger.error(box)
+    return box
+
+class addFormAddr(forms.ModelForm):
+    action = forms.CharField(widget=forms.HiddenInput(), initial="subAddr", required=True)
+    mac_addr = forms.ChoiceField(choices=list_mac_addr)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_action = ''
+        self.helper.layout = Layout(Modal(Field('name'), Field('mac_addr'), Field('action'), Submit("submit", "Оправить", css_class='btn btn-primery float-end'),  css_id="addForm", title='Добавить новую запись'))
+
+    class Meta:
+        model = dirAddr
+        fields = ('name', 'mac_addr')
