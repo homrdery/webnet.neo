51eaba5d7c0e23804da035fc20ad26a4e9513b8f
Artem <ar.lubimov@gmail.com>
2024-10-18 21:15:04 +0300

    add static

--- static/Index/JSscripts/ScriptsForDir.js
+++ static/Index/JSscripts/ScriptsForDir.js
@@ -18,87 +18,87 @@ function getform () {
 //    });}
 
 
-//function tableinit () {
-//    var table = $('#myTable').DataTable({
-//    Layout: {topStart: 'search', topEnd: {
-//    buttons: [
-//        {
-//            text:      '<i class="fa fa-plus"></i>',
-//            attr:  {
-//                title: 'Add item',
-//                id: 'BtAdd'
-//                },
-//            titleAttr: 'Add item',
-//            action: function ( e, dt, node, config ) {
-//                alert( 'Button activated' );
-//                }
-//        },
-//        {
-//            text:      '<i class="fa fa-pen"></i>',
-//            attr:  {
-//                title: 'Edit item',
-//                id: 'BtEdit'
-//                },
-//            init: function ( dt, node, config ) {
-//                this.disable();
-//            },
-//            action: function readdr( e, dt, node, config )  {
-//                var id = $('#myTable').DataTable.$('tr.selected')[0].id;
-//                console.log('re make user id ='+id);
-//                $("#Form").load("/getform.html?action=reAddr&mac_addr="+id, function(responseTxt, statusTxt, jqXHR)
-//                {
-//                    if(statusTxt == 'success'){
-//                            $('#addFormdel').modal();
-//                        }
-//                        if(statusTxt == 'error'){
-//                            alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
-//
-//                        }
-//                });
-//
-//            }
-//        },
-//        {
-//            text:      '<i class="fas fa-trash  aria-hidden="true"></i>',
-//            attr:  {
-//                title: 'Delete item',
-//                id: 'BtDelete'
-//            },
-//
-//            init: function ( dt, node, config ) {
-//                this.disable();
-//            },
-//            action: function deladdr( e, dt, node, config )  {
-//                var id = $('#myTable').DataTable.$('tr.selected')[0].id;
-//                console.log('Выбран пользователь под id ='+id);
-//                $("#Form").load("/getform.html?action=delAddr&mac_addr="+id, function(responseTxt, statusTxt, jqXHR)
-//                {
-//                    if(statusTxt == 'success'){
-//                            $('#addFormdel').modal();
-//                        }
-//                        if(statusTxt == 'error'){
-//                            alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
-//
-//                        }
-//                });
-//
-//        }
-//
-//        }
-//    ]}},
-//    pageLength: 25,
-//    select: true,
-//    rowId: 'id',
-//    language: {
-//        url:"/static/datatables/ru.json"
-//        },
-//});
-//    table.on( 'select deselect', function () {
-//        var selectedRows = table.rows( { selected: true } ).count();
-//        table.buttons(['#BtEdit']).enable( selectedRows === 1 );
-//        table.buttons(['#BtDelete']).enable( selectedRows > 0 );
-//    });
-//}
+function tableinit () {
+    var table = $('#myTable').DataTable({
+    Layout: {topStart: 'search', topEnd: {
+    buttons: [
+        {
+            text:      '<i class="fa fa-plus"></i>',
+            attr:  {
+                title: 'Add item',
+                id: 'BtAdd'
+                },
+            titleAttr: 'Add item',
+            action: function ( e, dt, node, config ) {
+                alert( 'Button activated' );
+                }
+        },
+        {
+            text:      '<i class="fa fa-pen"></i>',
+            attr:  {
+                title: 'Edit item',
+                id: 'BtEdit'
+                },
+            init: function ( dt, node, config ) {
+                this.disable();
+            },
+            action: function readdr( e, dt, node, config )  {
+                var id = $('#myTable').DataTable.$('tr.selected')[0].id;
+                console.log('re make user id ='+id);
+                $("#Form").load("/getform.html?action=reAddr&mac_addr="+id, function(responseTxt, statusTxt, jqXHR)
+                {
+                    if(statusTxt == 'success'){
+                            $('#addFormdel').modal();
+                        }
+                        if(statusTxt == 'error'){
+                            alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
+
+                        }
+                });
+
+            }
+        },
+        {
+            text:      '<i class="fas fa-trash  aria-hidden="true"></i>',
+            attr:  {
+                title: 'Delete item',
+                id: 'BtDelete'
+            },
+
+            init: function ( dt, node, config ) {
+                this.disable();
+            },
+            action: function deladdr( e, dt, node, config )  {
+                var id = $('#myTable').DataTable.$('tr.selected')[0].id;
+                console.log('Выбран пользователь под id ='+id);
+                $("#Form").load("/getform.html?action=delAddr&mac_addr="+id, function(responseTxt, statusTxt, jqXHR)
+                {
+                    if(statusTxt == 'success'){
+                            $('#addFormdel').modal();
+                        }
+                        if(statusTxt == 'error'){
+                            alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
+
+                        }
+                });
+
+        }
+
+        }
+    ]}},
+    pageLength: 25,
+    select: true,
+    rowId: 'id',
+    language: {
+        url:"/static/datatables/ru.json"
+        },
+});
+    table.on( 'select deselect', function () {
+        var selectedRows = table.rows( { selected: true } ).count();
+        table.buttons(['#BtEdit']).enable( selectedRows === 1 );
+        table.buttons(['#BtDelete']).enable( selectedRows > 0 );
+    });
+}
 
 $(tableinit);
 
