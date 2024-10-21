de54fec7db2c5c1baad8a45e2255c061ee174b70
Artem <ar.lubimov@gmail.com>
2024-10-21 18:09:41 +0300

    add static

--- src/webnet/index/urls.py
+++ src/webnet/index/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib import admin
 from index import views
-from logs import views
+from logs.views import logs
 urlpatterns = [
     path('', views.index),
     path('Addr.html', views.addr),
     path('getform.html', views.getform),
-    path('logs', views.logs),
+    path('logs', logs),
 
 ]
