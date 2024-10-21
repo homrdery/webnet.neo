19787923caf56539469681a1409d97c329f01dd1
Artem <ar.lubimov@gmail.com>
2024-10-21 17:46:14 +0300

    add static

--- src/webnet/index/urls.py
+++ src/webnet/index/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from index import views
 
 urlpatterns = [
-    path('', views.index, views.addr),
+    path('', views.index),
     path('Addr.html', views.addr),
     path('getform.html', views.getform),
 
